5d2bff35246463d818397c70cac1b0dd16732909
deeksha01032023 <deekshapandey@cedcoss.com>
2023-04-04 19:15:42 +0530

    final commit

---
+++ src/index.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+// for($i=0;$i<7;$i++)
+// {
+//     echo $n;
+    
+//     for($j=0;$j<7;$j++)
+//      {
+       
+//      }
+
+//      echo "<br>";
+$val=4;
+for($i=0;$i<2*$val;$i++)
+{ 
+    for($j=0;$j<(2*$val);$j++)
+    {
+        if()
+        echo "$val";
+        
+    }
+    echo "<br>";
+    // $val=$val-1;
+}
+
+?>
